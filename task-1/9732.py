92c1eecacd155f054c350a66608ebac7d5317f09
Supersonic <polarydid@gmail.com>
2025-12-19 17:21:30 +0200

    https://kompege.ru/variant?kim=25137209 тип 1 практика 19дек2025

---
+++ task-1/9732.py
@@ -0,0 +1,10 @@
+from itertools import permutations
+
+
+graph = 'FC CG GA AD DB BF EB EF EC EG'.split()
+matrix = '47 357 2567 16 236 345 123'.split()
+
+print(*range(1, 8))
+for i in permutations('ABCDEFG'):
+    if all(str(i.index(x) + 1) in matrix[i.index(y)] for x, y in graph):
+        print(*i)
