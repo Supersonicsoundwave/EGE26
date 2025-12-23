e2071b40974d4640442ab6c7af350cb57ec0eb38
Supersonic <polarydid@gmail.com>
2025-12-23 16:42:23 +0200

    https://kompege.ru/variant?kim=25138395 ср тип 1 8 14 23дек2025

--- task-14/21900.py
+++ task-14/21900.py
@@ -6,7 +6,7 @@ def convert(x, q):
     return res[:-1]
 
 
-for x in range(1, 2300):
+for x in range(1, 2301):
     f = convert(7**350 + 7**150 - x, 7)
     if f.count('0') == 200:
         print(x)
