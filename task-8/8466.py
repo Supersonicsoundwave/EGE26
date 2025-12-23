b12e834d9c13cb8d0f667e681a8b20959ae5b755
Supersonic <polarydid@gmail.com>
2025-12-23 16:41:15 +0200

    https://kompege.ru/variant?kim=25138395 ср тип 1 8 14 23дек2025

--- task-8/8466.py
+++ task-8/8466.py
@@ -6,8 +6,8 @@ alph = printable[:7]
 cnt = 0
 for val in product(alph, repeat=6):
     val = ''.join(val)
-    if val != '0':
-        if val[-1] not in '0123' and sum([1 if int(x) % 2 != 0 else 0 for x in val]) == 3:
+    if val[0] != '0':
+        if val[-1] not in '0123' and sum(int(int(x) % 2) == 0 for x in val) == 3:
             cnt += 1
 print(cnt)
 
