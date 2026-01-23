d427bd10408fab3fa2f78e7da8c749601000baf1
Supersonic <polarydid@gmail.com>
2026-01-23 16:46:29 +0200

    https://kompege.ru/variant?kim=25146071 интересная ср

--- task-8/4560.py
+++ task-8/4560.py
@@ -5,6 +5,7 @@ cnt = 0
 for val in set(permutations('ТИХОРЕЦК', r=4)):
     val = ''.join(val)
     cond1 = sum([1 for symb in val[:4] if symb == 'ТИХО'[val.index(symb)]]) == 2
+    cond1_1 = sum([1 for i in range(len(val[:4])) if val[i] == 'ТИХО'[i]]) == 2
     for i in 'ИОЕ':
         val = val.replace(i, '*')
     cond2 = val.count('*') == 2
