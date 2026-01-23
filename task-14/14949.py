21d11bacb804946c813bf15c5a726dc0e8360ae3
Supersonic <polarydid@gmail.com>
2026-01-23 16:41:02 +0200

    https://kompege.ru/variant?kim=25146071 интересная ср

--- task-14/14949.py
+++ task-14/14949.py
@@ -5,5 +5,5 @@ for x in range(1, 8):
     a = int(f'{x}1{x}', 16)
     b = int(f'{x}3{x}3', 8)
     f = a + b
-    if log(f, 2) % 1 == 0:
+    if log(f, 2) % 1 == 0 or bin(f)[2:].count('1') == 1:  # крутая идея от тимофея
         print(x)
