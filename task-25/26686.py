3bf6cc0b23323921b0219b3965baee78a139b009
Майя Мукатова <polarydid@gmail.com>
2026-02-06 17:54:24 +0200

    https://kompege.ru/variant?kim=25151985 тип 25 с множителями 6фев2026

---
+++ task-25/26686.py
@@ -0,0 +1,28 @@
+def fact(num):
+    d = []
+    while num % 2 == 0:
+        d += [2]
+        num //= 2
+
+    i = 3
+    while i * i < num:
+        while num % i == 0:
+            d += [i]
+            num //= i
+        i += 2
+
+    if num > 2:
+        d += [num]
+
+    return d
+
+
+cnt = 0
+for n in range(89428304 + 1, 10 ** 20):
+    d = fact(n)
+    if len(d) >= 6 and n % sum(d) == 0:
+        print(n, sum(d))
+        cnt += 1
+        if cnt == 6:
+            break
+
