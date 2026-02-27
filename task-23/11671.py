5e7c9accaa333551bd04653c459bd254d4ddd272
Майя Мукатова <polarydid@gmail.com>
2026-02-27 16:31:29 +0200

    https://kompege.ru/variant?kim=25158145 дз на 24фев2026 тип 23

--- task-23/11671.py
+++ task-23/11671.py
@@ -1,15 +1,22 @@
-spots = set()
+def f1(cur, cnt=0):
+    if cnt == 15:
+        return {cur}
+    return f1(cur + 10, cnt + 1) | f1(cur - 5, cnt + 1)
 
+##############################################################
+
+print(len(f1(1)))
 
 def f(cur, cnt=0):
     global spots
-    if cnt > 15:
-        return 0
     if cnt == 15:
         spots |= {cur}
-        return 1
-    return f(cur + 10, cnt + 1) + f(cur - 5, cnt + 1)
+        return
+    f(cur + 10, cnt + 1)
+    f(cur - 5, cnt + 1)
 
+
+spots = set()
 
 f(1)
 print(len(spots))
--- task-23/2341.py
+++ task-23/2341.py
@@ -1,15 +1,28 @@
-spots = set()
-
-
 def f(cur, cnt=0):
     global spots
     if cnt == 8:
         if 1000 <= cur <= 1024:
             spots |= {cur}
-            return 1
-        return 0
-    return f(cur + 1, cnt + 1) + f(cur + 5, cnt + 1) + f(cur * 3, cnt + 1)
+            return
+        return
+    f(cur + 1, cnt + 1)
+    f(cur + 5, cnt + 1)
+    f(cur * 3, cnt + 1)
 
+
+spots = set()
 
 f(1)
 print(len(spots))
+
+########################################################################
+
+def f1(cur, cnt=0):
+    if cnt == 8:
+        if 1000 <= cur <= 1024:
+            return {cur}
+        return set()
+    return f1(cur + 1, cnt + 1) | f1(cur + 5, cnt + 1) | f1(cur * 3, cnt + 1)
+
+
+print(len(f1(1)))
