59f52be177ce4204f5c78b360b5089f0db4c8b93
Supersonic <polarydid@gmail.com>
2026-03-27 17:05:43 +0200

    https://kompege.ru/variant?kim=25169522 тип 17 пары не стоят рядом

---
+++ task-17/5758_1.py
@@ -0,0 +1,12 @@
+with open(r'.\files\17_5758.txt') as file:
+    data = [int(i) for i in file]
+
+moda = max((data.count(num), num) for num in data)[-1]
+ans = []
+for i in range(len(data)):
+    for j in range(i + 1, len(data)):
+        nums = data[i], data[j]
+        if min(nums) < moda < max(nums) and (j - i - 1) % 2 != 0:
+            ans += [abs(moda - min(nums))]
+
+print(len(ans), max(ans))
