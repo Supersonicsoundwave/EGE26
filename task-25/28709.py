8c9ae4d73adecd90a1e2ccdb7dbbce7e53d911cd
Supersonic <polarydid@gmail.com>
2026-04-17 16:10:38 +0200

    исправлена ошибка из-зи которой падала задача

--- task-25/28709.py
+++ task-25/28709.py
@@ -1,12 +1,3 @@
-def is_prime(num):
-    if num < 2:
-        return False
-    for i in range(2, int(num ** .5) + 1):
-        if num % i == 0:
-            return False
-    return True
-
-
 def fact(num):
     res = []
     while num % 2 == 0:
@@ -18,6 +9,7 @@ def fact(num):
         while num % i == 0:
             res += [i]
             num //= i
+        i += 2
 
     if num > 2:
         res += [num]
@@ -27,19 +19,16 @@ def fact(num):
 
 def f(num):
     m = fact(num)
-    if len(m) == 3 and all(is_prime(mn) for mn in m):
+    if len(m) == 3:
         if sum(1 for mn in m if '3' in str(mn) or '4' in str(mn)) == 3:
             return max(m)
     return 0
 
 
 cnt = 0
-print(fact(6300051))
-# for n in range(6_300_000 + 1, 10 ** 20):
-#     if mm := f(n):
-#         print(n, mm)
-#         cnt += 1
-#         if cnt == 5:
-#             break
-#
-
+for n in range(6_300_000 + 1, 10 ** 20):
+    if mm := f(n):
+        print(n, mm)
+        cnt += 1
+        if cnt == 5:
+            break
