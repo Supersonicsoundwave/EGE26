c3d56cb0af66831a2cc22678b04d0b9aa4edce89
Supersonic <polarydid@gmail.com>
2026-05-15 17:13:32 +0200

    сложая задачка из домашки проблема устранена

--- task-24/21717.py
+++ task-24/21717.py
@@ -3,11 +3,11 @@ with open(r'.\files\24_21717.txt') as file:
 
 data = data.split('RSQ')
 ans = 10**20
-for i in range(1, len(data) - 128):
-    line = 'RSQ' + 'RSQ'.join(data[i:i + 130]) + 'RSQ'
-    dop = 'RSQ'.join(data[i + 130:])
+for i in range(1, len(data) - 130):
+    line = 'RSQ' + 'RSQ'.join(data[i:i + 129]) + 'RSQ'
+    dop = data[i + 129] + 'RSQ'
     for j in 'RSFGW':
         dop = dop.replace(j, '*')
-    line += dop[:dop.find('*')]
+    line += dop[:dop.find('*') + 1]
     ans = min(ans, len(line))
 print(ans)
