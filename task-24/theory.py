c6ba55117feea783b821bf17c4cc8779955a652b
Майя Мукатова <polarydid@gmail.com>
2026-05-21 15:45:02 +0200

    теория тип 24 умное решение

---
+++ task-24/theory.py
@@ -0,0 +1,17 @@
+# умный способ
+with open(r'..\..\files\24_21717.txt') as file:
+    data = file.readline()
+
+data = data.replace('RSQ', 'Rsq rsQ')
+data = data.split()
+
+ans = 10 ** 10
+for i in range(len(data) - 128 - 1):
+    line = ''.join(data[i:i + 129]).replace('sqrs', 'S')
+    cnt = 0
+    for j in data[i + 129][3:]:
+        cnt += 1
+        if j not in 'Qq':
+            break
+    ans = min(ans, len(line) + cnt)
+print(ans)
